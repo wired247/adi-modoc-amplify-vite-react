7821ba11263b920017f80c9a89d22be8c48ebbd3
wired247 <willoughby.d@gmail.com>
2026-09-09 10:13:51 -0400

    Update supabase URL and publishable key

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // These should come from environment variables
-const supabaseUrl = 'https://xnrpuanrcmnmkhvhiqnb.supabase.co'
-const supabaseAnonKey = 'sb_publishable_aWxkeFrMTqfLIPczx5UwHw_as-C_N0r'
+const supabaseUrl = 'https://pyaxgiswlufuooilckrd.supabase.co'
+const supabaseAnonKey = 'sb_publishable_mx50vJUH9zdg6tYXwwb7gQ_GgdGeH4k'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
